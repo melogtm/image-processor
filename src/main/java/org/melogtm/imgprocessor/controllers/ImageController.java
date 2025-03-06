ed05ab90ab8777353419c3a7bc3441516ed34867
melogtm <gabrielcursos@proton.me>
2025-03-06 16:51:16 -0300

    get images and temp db

--- src/main/java/org/melogtm/imgprocessor/controllers/ImageController.java
+++ src/main/java/org/melogtm/imgprocessor/controllers/ImageController.java
@@ -6,14 +6,33 @@ import org.melogtm.imgprocessor.domain.image.transformation.Transformations;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
+import java.io.File;
+import java.io.IOException;
 import java.util.UUID;
 
 @RestController()
 @RequestMapping("/image")
 public class ImageController {
 
+    /*
+    I'll use a temporary folder as my DB, future me will implement S3 buckets and so, I just
+    want to transform mah images
+     */
+    private static final String IMAGE_FOLDER = System.getProperty("user.dir") + "/src/main/resources/images/";
+
     @PostMapping(value = "/", consumes = "multipart/form-data")
     public ResponseEntity<ImageResponseDTO> processImage(@ModelAttribute ImageRequestDTO imageRequestDTO) {
+        String filename = imageRequestDTO.image().getOriginalFilename();
+
+        File file = new File(IMAGE_FOLDER + filename);
+
+        try {
+            file.createNewFile();
+            imageRequestDTO.image().transferTo(file);
+        } catch (IOException e) {
+            System.out.println("Sorry, can't save image: " + e.getMessage());
+        }
+
         return ResponseEntity.ok(new ImageResponseDTO("http://localhost:8080/image/1", "metadata"));
     }
 
---
+++ src/main/java/org/melogtm/imgprocessor/services/ImageService.java
@@ -0,0 +1,10 @@
+package org.melogtm.imgprocessor.services;
+
+import org.springframework.stereotype.Service;
+
+@Service
+public class ImageService {
+
+
+
+}
